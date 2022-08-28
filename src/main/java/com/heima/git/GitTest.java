d0b0ed119069c36ba81ad6b1d39dc8da388ac1f0
zhuoguizhi <15737151987@163.com>
2022-08-28 17:30:00 +0800

    hot-fix 3 commit

--- src/main/java/com/heima/git/GitTest.java
+++ src/main/java/com/heima/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello.git2");
         System.out.println("hello.git3");
         System.out.println("你好世界");
+        System.out.println("热修分支进行了修改");
     }
 }
